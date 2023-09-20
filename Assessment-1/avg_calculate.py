22e2274b6adcbade4114bf82a8db2be76a20fd05
Arudhran <72651851+ArudhranPK@users.noreply.github.com>
2023-09-20 13:54:49 +0530

    Update avg_calculate.py

--- Assessment-1/avg_calculate.py
+++ Assessment-1/avg_calculate.py
@@ -1,9 +1,21 @@
 marks = []
+
 for i in range(1, 6):
+    marks.append([])
+    
     for j in range(1, 6):
         while True:
             try:
-                marks[i-1][j-1] = float(input("Enter the  subject", j,  "mark of student", i, ": "))
+                marks[i-1].append(float(input(f"Enter the subject {j} mark of student {i} : ")))
+                break
 
             except:
                 print("The input entered is not valid!!!!!\n")
+    print()
+
+total_avg = 0
+for k in range(1, 6):
+    print("The average marks of student", i, "is", sum(marks[k-1])/5)
+    total_avg += sum(marks[k-1])/5
+
+print("\nThe average marks of all five students is", total_avg/5)
