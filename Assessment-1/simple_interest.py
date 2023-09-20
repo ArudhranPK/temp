d5e43b5546712476e45b8a379392d89d8ec667ac
Arudhran <72651851+ArudhranPK@users.noreply.github.com>
2023-09-20 23:43:48 +0530

    Update simple_interest.py

--- Assessment-1/simple_interest.py
+++ Assessment-1/simple_interest.py
@@ -3,7 +3,7 @@ while True:
         amou = float(input("Enter the princtipal amount : "))
         inte = float(input("Enter the rate of interest per year : "))
         time = float(input("Enter the time in years : "))
-        si =  (amou, inte, time)/100
+        si =  (amou * inte * time)/100
         print("\nThe simple interest for the given values is :", si)
         break
         
