7c4d7afc4f9725cfd86fe412005e6a041381517f
Arudhran <72651851+ArudhranPK@users.noreply.github.com>
2023-09-20 23:09:03 +0530

    Update simple_interest.py

--- Assessment-1/simple_interest.py
+++ Assessment-1/simple_interest.py
@@ -4,24 +4,11 @@ def cal_simple_interest(amount, interest, time):
 while True:
     try:
         amou = float(input("Enter the princtipal amount : "))
-
-        while True:
-            try:
-                inte = float(input("Enter the rate of interest per year : "))
-
-                while True:
-                    try:
-                        time = float(input("Enter the time in years : "))
-                        print("\nThe simple interest for the given values is :", cal_simple_interest(amou, inte, time))
-                        break
-                
-                    except:
-                        print("The input entered is not valid!!!!!\n")
-                break
-
-            except:
-                print("The input entered is not valid!!!!!\n")
+        inte = float(input("Enter the rate of interest per year : "))
+        time = float(input("Enter the time in years : "))
+        si =  cal_simple_interest(amou, inte, time)
+        print("\nThe simple interest for the given values is :", si)
         break
-
+        
     except:
         print("The input entered is not valid!!!!!\n")
