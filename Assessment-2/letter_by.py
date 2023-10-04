8a920b1311a358d1e8688b5a36596096f1282e9d
Arudhran <72651851+ArudhranPK@users.noreply.github.com>
2023-10-04 22:06:24 +0530

    improv

--- Assessment-2/letter_by.py
+++ Assessment-2/letter_by.py
@@ -1,14 +1,9 @@
-#def letter_by_letter(word):
-#    for i in range(1, len(word) + 1):
-#        print(word[0:i:])
-#    for i in range(len(word) - 1, 0, -1):
-#        print(word[0:i:])
 
 def letter_by_letter(word):
     for i in range(1, len(word) + 1):
-        print(" " * (len(word) - i) + word[0:i:])
+        print(" " * (len(word) - i) + word[i-1::-1])
     for i in range(len(word) - 1, 0, -1):
-        print(" " * (len(word) - i) + word[0:i:])
+        print(" " * (len(word) - i) + word[i-1::-1])
 
 inp = input("Enter a word to print the letters : ")
 letter_by_letter(inp)
